3680700a0c32c0c7752837e7350ed777c0f94c19
Omotola Macaulay <tolamacaulay@gmail.com>
2021-08-06 22:40:21 +0100

    Create user Item

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import UserItem from './components/users/UserItem';
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
