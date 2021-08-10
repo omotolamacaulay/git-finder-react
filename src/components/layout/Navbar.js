67a247f897378c2b49f8d30bc98a8283f3d43126
Omotola Macaulay <tolamacaulay@gmail.com>
2021-08-10 12:46:44 +0100

    fetch users from github api

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,24 +1,21 @@
-import { Component } from 'react'
 import PropTypes from 'prop-types'
 
-export class Navbar extends Component {
-  static defaultProps = {
-      title: 'Github Finder',
-      icon: 'fab fa-github'
-  };
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-  };
-  render() {
+const Navbar = ({ icon, title }) => {
+
     return (
       <nav className="navbar bg-primary">
         <h1>
-          <i className={this.props.icon}></i> {this.props.title}
+          <i className={icon}></i> {title}
         </h1>
       </nav>
     )
-  }
 }
-
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fab fa-github'
+};
+Navbar.propTypes = {
+title: PropTypes.string.isRequired,
+icon: PropTypes.string.isRequired
+};
 export default Navbar
